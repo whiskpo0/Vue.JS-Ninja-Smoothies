19ef25a4df80490df1e21eed432d619c98a9c854
whiskpo0 <whiskypo082@gmail.com>
2019-03-19 06:56:58 -0500

    changed firebase init.js file for security reasons

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,12 +4,12 @@
 
  // Initialize Firebase
  var config = {
-    apiKey: "AIzaSyCb8x17s6_zvc2Z0hZfLVHhpZUy5DPj7QA",
-    authDomain: "ninja-smoothies-c81be.firebaseapp.com",
-    databaseURL: "https://ninja-smoothies-c81be.firebaseio.com",
-    projectId: "ninja-smoothies-c81be",
-    storageBucket: "ninja-smoothies-c81be.appspot.com",
-    messagingSenderId: "749912863005"
+    apiKey: "00000000000000000000000000000000000000000",
+    authDomain: "YourDomainInformationHere",
+    databaseURL: "https://yourProjectInfoHere.firebaseio.com",
+    projectId: "YourPrjectIDGoesHere",
+    storageBucket: "YouNeedToEnterYourInfoHere",
+    messagingSenderId: "000000000000"
   };
 
   const firebaseApp = firebase.initializeApp(config);
